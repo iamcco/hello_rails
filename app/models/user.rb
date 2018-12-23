5e6a556979f93cc5d9f9c5e6f70a6020bf6af84f
iamcco <ooiss@qq.com>
2018-12-23 17:03:40 +0800

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :microposts
   validates :name, presence: true
   validates :email, presence: true
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
